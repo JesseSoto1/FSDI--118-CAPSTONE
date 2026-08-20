28abdf5c1e30202ff3aa7a94e3e34abacfb41f06
Jesse <jessechuy1985@gmail.com>
2026-08-20 16:51:09 -0700

    new_style

--- config/settings.py
+++ config/settings.py
@@ -121,11 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 #Media FIles
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 
 LOGIN_REDIRECT_URL = 'home'
